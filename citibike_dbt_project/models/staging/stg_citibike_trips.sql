329cecbafa14fca7d0e4a51fa95ad422561bb396
Yasser Sakr <yasser.mansour88@gmail.com>
2025-04-05 16:47:42 +0000

    sync

--- citibike_dbt_project/models/staging/stg_citibike_trips.sql
+++ citibike_dbt_project/models/staging/stg_citibike_trips.sql
@@ -1,48 +1,106 @@
+{{
+  config(
+    materialized='view',
+    schema='staging'
+  )
+}}
+
 with trips_2023 as (
     select 
-        trip_id,
-        cast(start_time as timestamp) as start_time,
-        cast(end_time as timestamp) as end_time,
+        ride_id,
+        lower(rideable_type) as rideable_type,
+        cast(started_at as timestamp) as start_time,
+        cast(ended_at as timestamp) as end_time,
+        start_station_name,
         start_station_id,
+        end_station_name,
         end_station_id,
-        lower(user_type) as user_type,
-        timestamp_diff(end_time, start_time, minute) as trip_duration_minutes,
+        start_lat,
+        start_lng,
+        end_lat,
+        end_lng,
+        lower(member_casual) as member_casual,
+        timestamp_diff(cast(ended_at as timestamp), cast(started_at as timestamp), minute) as trip_duration_minutes,
+        round(
+            6371 * 2 * asin(
+                sqrt(
+                    power(sin((end_lat - start_lat) * acos(-1) / 360), 2) + 
+                    cos(start_lat * acos(-1) / 180) * 
+                    cos(end_lat * acos(-1) / 180) * 
+                    power(sin((end_lng - start_lng) * acos(-1) / 360), 2)
+                )
+            ), 2
+        ) as trip_distance_km,
         '2023' as year
-    from {{ source('citibike_2023', 'citibike_2023') }}
-    where trip_id is not null 
-      and start_time is not null 
-      and end_time is not null
+    from {{ source('zoomcamp_project', 'citibike_2023_partitioned') }}
+    where ride_id is not null
 ),
 
 trips_2024 as (
     select 
-        trip_id,
-        cast(start_time as timestamp) as start_time,
-        cast(end_time as timestamp) as end_time,
+        ride_id,
+        lower(rideable_type) as rideable_type,
+        cast(started_at as timestamp) as start_time,
+        cast(ended_at as timestamp) as end_time,
+        start_station_name,
         start_station_id,
+        end_station_name,
         end_station_id,
-        lower(user_type) as user_type,
-        timestamp_diff(end_time, start_time, minute) as trip_duration_minutes,
+        start_lat,
+        start_lng,
+        end_lat,
+        end_lng,
+        lower(member_casual) as member_casual,
+        timestamp_diff(cast(ended_at as timestamp), cast(started_at as timestamp), minute) as trip_duration_minutes,
+        round(
+            6371 * 2 * asin(
+                sqrt(
+                    power(sin((end_lat - start_lat) * acos(-1) / 360), 2) + 
+                    cos(start_lat * acos(-1) / 180) * 
+                    cos(end_lat * acos(-1) / 180) * 
+                    power(sin((end_lng - start_lng) * acos(-1) / 360), 2)
+                )
+            ), 2
+        ) as trip_distance_km,
         '2024' as year
-    from {{ source('citibike_2024', 'citibike_2024') }}
-    where trip_id is not null 
-      and start_time is not null 
-      and end_time is not null
-),
-
-combined as (
-    select * from trips_2023
-    union all
-    select * from trips_2024
+    from {{ source('zoomcamp_project', 'citibike_2024_partitioned') }}
+    where ride_id is not null
 )
 
 select 
-    trip_id,
+    ride_id,
+    rideable_type,
     start_time,
     end_time,
+    start_station_name,
     start_station_id,
+    end_station_name,
     end_station_id,
-    user_type,
+    start_lat,
+    start_lng,
+    end_lat,
+    end_lng,
+    member_casual,
     trip_duration_minutes,
+    trip_distance_km,
     year
-from combined;
+from trips_2023
+union all
+select 
+    ride_id,
+    rideable_type,
+    start_time,
+    end_time,
+    start_station_name,
+    start_station_id,
+    end_station_name,
+    end_station_id,
+    start_lat,
+    start_lng,
+    end_lat,
+    end_lng,
+    member_casual,
+    trip_duration_minutes,
+    trip_distance_km,
+    year
+from trips_2024
